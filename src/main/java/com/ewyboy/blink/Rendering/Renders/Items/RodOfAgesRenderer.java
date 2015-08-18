19c5b70444e656da99b3fffcc98a17306973b6a4
EwyBoy <EwyBoy@gmail.com>
2015-08-19 01:14:35 +0200

    Better 3D rendering

--- src/main/java/com/ewyboy/blink/Rendering/Renders/Items/RodOfAgesRenderer.java
+++ src/main/java/com/ewyboy/blink/Rendering/Renders/Items/RodOfAgesRenderer.java
@@ -2,6 +2,7 @@ package com.ewyboy.blink.Rendering.Renders.Items;
 
 import com.ewyboy.blink.Networking.ClientProxy;
 import com.ewyboy.blink.Rendering.Models.Items.RodOfAgesModel;
+import com.ewyboy.blink.Utillity.Logger;
 import com.ewyboy.blink.Utillity.StringMap;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.OpenGlHelper;
@@ -39,30 +40,41 @@ public class RodOfAgesRenderer implements IItemRenderer {
         return helper != ItemRendererHelper.ENTITY_BOBBING && helper != ItemRendererHelper.ENTITY_ROTATION;
     }
 
+    private float rotate = 0.0f;
+
     @Override
     public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
         ItemStack enderEye = new ItemStack(Items.ender_eye, 1, 0);
         EntityItem entity = new EntityItem(Minecraft.getMinecraft().theWorld, 0D, 0D, 0D, enderEye);
 
-        GL11.glPushMatrix();
-            entity.hoverStart = 0.0F;
+        if (type.equals(type.EQUIPPED_FIRST_PERSON)) {
+            GL11.glPushMatrix();
+            entity.hoverStart = -1.0F;
             RenderItem.renderInFrame = true;
-                GL11.glScalef(1.0f,1.0f,1.0f);
-                GL11.glTranslatef(0.35f,2.35f,0.4f);
-                GL11.glRotatef(180, 1, 0, 1);
-                GL11.glColor3f(0.0F,1.0F,0.0F);
-                OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 242, 242);
+            GL11.glScalef(1.0f,1.0f,1.0f);
+            if(ClientProxy.shiftPressed()) {
+                GL11.glTranslatef(-0.4f,2.375f,-0.4f);
+            } else {
+                GL11.glTranslatef(0.35f,2.25f,0.4f);
+            }
+            rotate+=0.1f;
+            if (rotate>1) {rotate=-1;}
+            GL11.glRotatef(180, rotate, 0, 1);
+            GL11.glColor3f(0.0F,1.0F,0.0F);
+            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 242, 242);
             RenderManager.instance.renderEntityWithPosYaw(entity, 0.0D, 0.0D, 0.0D, 0.0F, 0.0F);
             RenderItem.renderInFrame = false;
-        GL11.glPopMatrix();
-
+            GL11.glPopMatrix();
+        } else if (type.equals(type.EQUIPPED)) {
+            //Render Third Person later when time
+        }
 
         GL11.glPushMatrix();
-        if(type==type.EQUIPPED) {
+        if(type.equals(type.EQUIPPED)) {
             GL11.glScalef(-2.5f,-2.5f,2.5f);
             GL11.glTranslatef(-0.15f,-0.45f,0.0f);
             GL11.glRotatef(-90.0f,-1.0f,1.0f,0.0f);
-        } else if (type == type.EQUIPPED_FIRST_PERSON) {
+        } else if (type.equals(type.EQUIPPED_FIRST_PERSON)) {
             GL11.glScalef(-2.0f,-2.0f,2.0f);
             if(ClientProxy.shiftPressed()) {
                 GL11.glRotatef(180.0f,0.0f,1.0f,0.0f);
